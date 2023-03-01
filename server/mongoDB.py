6d366d1c37b6ddb4b000b2ba3c41cd5b461e9897
noambaum00 <105561406+noambaum00@users.noreply.github.com>
2023-03-01 15:13:38 +0200

    add permission to db

--- server/mongoDB.py
+++ server/mongoDB.py
@@ -75,10 +75,18 @@ class ChatDB:
 
     def get_user(self, username):
         return self.users.find_one({"_id": username})
+    
+    def add_permission(self, username, permission):
+        self.users.update_one({"_id": username}, {"$push": {"permissions": permission}})
 
-    def get_admin(self, username):
-        return self.users.find_one({"_id": username})["isadmin"]
+    def remove_permission(self, username, permission):
+        self.users.update_one({"_id": username}, {"$pull": {"permissions": permission}})
 
+    def get_permissions(self, username):
+        return self.users.find_one({"_id": username})["permissions"]
+
+    def is_admin(self, username):
+        return "admin" in self.get_permissions(username)
 
     def get_room(self, room_name):
         return self.rooms.find_one({"_id": room_name})
@@ -120,6 +128,166 @@ class ChatDB:
 
     def close(self):
         self.client.close()
+
+
+
+
+
+class mongo_permission:
+    def is_moderator(self, username):
+        return "moderator" in self.get_permissions(username)
+
+    def is_user(self, username):
+        return "user" in self.get_permissions(username)
+
+    def is_guest(self, username):
+        return "guest" in self.get_permissions(username)
+
+    def is_banned(self, username):
+        return "banned" in self.get_permissions(username)
+
+    def is_muted(self, username):
+        return "muted" in self.get_permissions(username)
+
+    def is_kicked(self, username):
+        return "kicked" in self.get_permissions(username)
+
+    def is_room_admin(self, username, room_name):
+        return username == self.get_room_admin(room_name)
+
+    def is_server_admin(self, username):
+        return username == self.get_server_admin(username)
+
+    def is_room_moderator(self, username, room_name):
+        return username in self.get_room_moderators(room_name)
+
+    def is_server_moderator(self, username):
+        return username in self.get_server_moderators(username)
+
+    def is_room_user(self, username, room_name):
+        return username in self.get_room_users(room_name)
+
+    def is_server_user(self, username):
+        return username in self.get_server_users(username)
+
+    def is_room_guest(self, username, room_name):
+        return username in self.get_room_guests(room_name)
+
+    def is_server_guest(self, username):
+        return username in self.get_server_guests(username)
+
+    def is_room_banned(self, username, room_name):
+        return username in self.get_room_banned(room_name)
+
+    def is_server_banned(self, username):
+        return username in self.get_server_banned(username)
+
+    def is_room_muted(self, username, room_name):
+        return username in self.get_room_muted(room_name)
+
+    def is_server_muted(self, username):
+        return username in self.get_server_muted(username)
+
+    def is_room_kicked(self, username, room_name):
+        return username in self.get_room_kicked(room_name)
+
+    def is_server_kicked(self, username):
+        return username in self.get_server_kicked(username)
+
+    def get_room_moderators(self, room_name):
+        return self.rooms.find_one({"_id": room_name})["moderators"]
+
+    def get_server_moderators(self, username):
+        return self.users.find_one({"_id": username})["moderators"]
+
+    def get_room_users(self, room_name):
+        return self.rooms.find_one({"_id": room_name})["users"]
+
+    def get_server_users(self, username):
+        return self.users.find_one({"_id": username})["users"]
+
+    def get_room_guests(self, room_name):
+        return self.rooms.find_one({"_id": room_name})["guests"]
+
+    def get_server_guests(self, username):
+        return self.users.find_one({"_id": username})["guests"]
+
+    def get_room_banned(self, room_name):
+        return self.rooms.find_one({"_id": room_name})["banned"]
+
+    def get_server_banned(self, username):
+        return self.users.find_one({"_id": username})["banned"]
+
+    def get_room_muted(self, room_name):
+        return self.rooms.find_one({"_id": room_name})["muted"]
+
+    def get_server_muted(self, username):
+        return self.users.find_one({"_id": username})["muted"]
+
+    def get_room_kicked(self, room_name):
+        return self.rooms.find_one({"_id": room_name})["kicked"]
+
+    def get_server_kicked(self, username):
+        return self.users.find_one({"_id": username})["kicked"]
+
+    def add_room_moderator(self, username, room_name):
+        self.rooms.update_one({"_id": room_name}, {"$push": {"moderators": username}})
+
+    def add_server_moderator(self, username, room_name):
+        self.users.update_one({"_id": username}, {"$push": {"moderators": username}})
+
+    def add_room_user(self, username, room_name):
+        self.rooms.update_one({"_id": room_name}, {"$push": {"users": username}})
+
+    def add_server_user(self, username, room_name):
+        self.users.update_one({"_id": username}, {"$push": {"users": username}})
+
+    def add_room_guest(self, username, room_name):
+        self.rooms.update_one({"_id": room_name}, {"$push": {"guests": username}})
+
+    def add_server_guest(self, username, room_name):
+        self.users.update_one({"_id": username}, {"$push": {"guests": username}})
+
+    def add_room_banned(self, username, room_name):
+        self.rooms.update_one({"_id": room_name}, {"$push": {"banned": username}})
+
+    def add_server_banned(self, username, room_name):
+        self.users.update_one({"_id": username}, {"$push": {"banned": username}})
+
+    def add_room_muted(self, username, room_name):
+        self.rooms.update_one({"_id": room_name}, {"$push": {"muted": username}})
+
+    def add_server_muted(self, username, room_name):
+        self.users.update_one({"_id": username}, {"$push": {"muted": username}})
+
+    def add_room_kicked(self, username, room_name):
+        self.rooms.update_one({"_id": room_name}, {"$push": {"kicked": username}})
+
+    def add_server_kicked(self, username, room_name):
+        self.users.update_one({"_id": username}, {"$push": {"kicked": username}})
+
+    def remove_room_moderator(self, username, room_name):
+        self.rooms.update_one({"_id": room_name}, {"$pull": {"moderators": username}})
+
+    def remove_server_moderator(self, username, room_name):
+        self.users.update_one({"_id": username}, {"$pull": {"moderators": username}})
+
+    def remove_room_user(self, username, room_name):
+        self.rooms.update_one({"_id": room_name}, {"$pull": {"users": username}})
+
+    def remove_server_user(self, username, room_name):
+        self.users.update_one({"_id": username}, {"$pull": {"users": username}})
+
+    def remove_room_guest(self, username, room_name):
+        self.rooms.update_one({"_id": room_name}, {"$pull": {"guests": username}})
+
+    def remove_server_guest(self, username, room_name):
+        self.users.update_one({"_id": username}, {"$pull": {"guests": username}})
+
+
+
+
+
 
 if __name__ == "__main__":
     chat_db = ChatDB("mongodb+srv://noambaum:noambaum@cluster0.ec4wlbs.mongodb.net/?retryWrites=true&w=majority")
