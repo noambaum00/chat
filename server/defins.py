b31f6ccb97be54b906284f4d4d57fafa90d06c6f
noambaum00 <105561406+noambaum00@users.noreply.github.com>
2023-05-04 16:15:14 +0300

    Update defins.py

--- server/defins.py
+++ server/defins.py
@@ -1,28 +1,27 @@
-class defin:
-    HELP_MASSAGE = """
-    the list comend is:
-    \tlsr: = list room
-    \tjnr: = join room
-    \tlvr: = leave room
-    \tlsur:= list users in room
-    \tmsg: = send message
-    \tadr: = add room to user list
-    \texit
-    \thelp
-    \tchange_password
-    """
-    ADMIN_COMMENDS= """
-    \tcrr: = crieit room
-    \tdlr: = delete room
-    \tdlu: = delete user
-    \tlsr: = list room
-    \tlsur:= list users in room
-    \tkcu: = kick user
-    \tachp: = admin change password
-    \text: = exit
-    """
+HELP_MASSAGE = """
+the list comend is:
+\tlsr: = list room
+\tjnr: = join room
+\tlvr: = leave room
+\tlsur:= list users in room
+\tmsg: = send message
+\tadr: = add room to user list
+\texit
+\thelp
+\tchange_password
+"""
+ADMIN_COMMENDS= """
+\tcrr: = crieit room
+\tdlr: = delete room
+\tdlu: = delete user
+\tlsr: = list room
+\tlsur:= list users in room
+\tkcu: = kick user
+\tachp: = admin change password
+\text: = exit
+"""
 
-    LIST_ROOM = "crr"
-    JOIN_ROOM = "jnr"
-    LEAVE_ROOM = "lvr"
-    LIST_USERS_IN_ROOM = "lsur"
+LIST_ROOM = "crr"
+JOIN_ROOM = "jnr"
+LEAVE_ROOM = "lvr"
+LIST_USERS_IN_ROOM = "lsur"
